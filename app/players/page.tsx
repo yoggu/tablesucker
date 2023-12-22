165ffa7184f3afa4a32fc42d97018899f2f9631b
Yannick Bättig <yannick.baettig@netnode.ch>
2023-12-22 19:44:59 +0100

    Add Supabase integration to Players page

--- app/players/page.tsx
+++ app/players/page.tsx
@@ -1,8 +1,18 @@
+import { createClient } from "@/utils/supabase/server";
+import { cookies } from "next/headers";
 
 export default async function Players() {
+  const cookieStore = cookies();
+  const supabase = createClient(cookieStore);
+  const { data, error } = await supabase.from("players").select("*");
+
+  console.log(data);
   return (
     <div>
       Players
+      {data?.map((player) => (
+        <div key={player.id}>{player.name}</div>
+      ))}
     </div>
-  )
+  );
 }
