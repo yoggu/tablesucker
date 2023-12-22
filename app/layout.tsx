a0275edff7eb439c244cb55bfc2fe82bfa033f76
Yannick Bättig <yannick.baettig@netnode.ch>
2023-12-22 14:20:20 +0100

    remove stuff

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,8 +7,8 @@ const defaultUrl = process.env.VERCEL_URL
 
 export const metadata = {
   metadataBase: new URL(defaultUrl),
-  title: 'Next.js and Supabase Starter Kit',
-  description: 'The fastest way to build apps with Next.js and Supabase',
+  title: 'tablesucker',
+  description: 'See who sucks at table soccer',
 }
 
 export default function RootLayout({
@@ -18,7 +18,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className={GeistSans.className}>
-      <body className="bg-background text-foreground">
+      <body className="dark:bg-slate-800">
         <main className="min-h-screen flex flex-col items-center">
           {children}
         </main>
