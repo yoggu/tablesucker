8b42abfb606b955db4bda5be21b4fb2c30ce3dcf
Yannick Bättig <yannick.baettig@netnode.ch>
2023-12-23 22:56:07 +0100

    Add ModeToggle component to layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import Menu from "@/components/layout/menu";
 import { ThemeProvider } from "@/components/layout/theme-provider";
 import Header from "@/components/layout/header";
 import { Toaster } from "@/components/ui/toaster";
+import { ModeToggle } from "@/components/layout/mode-toggle";
 
 const defaultUrl = process.env.VERCEL_URL
   ? `https://${process.env.VERCEL_URL}`
@@ -22,13 +23,14 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className={GeistSans.className} suppressHydrationWarning>
-      <body className="flex min-h-screen dark:bg-slate-800">
+      <body className="flex min-h-screen dark:bg-slate-950">
         <ThemeProvider
           attribute="class"
           defaultTheme="system"
           enableSystem
           disableTransitionOnChange
         >
+          {/* <ModeToggle /> */}
           <aside className="pt-4 px-4">
             <Menu />
           </aside>
