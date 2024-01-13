bbf52b9bbe7bb5657e88efe88b1af7bf513aafbb
Yannick Bättig <yannick.baettig@netnode.ch>
2024-01-13 22:34:52 +0100

    Update AvatarFallback delayMs

--- components/ui/player-avatar.tsx
+++ components/ui/player-avatar.tsx
@@ -21,7 +21,7 @@ export default function PlayerAvatar({
     <div className="relative flex w-fit items-center gap-3">
       <Avatar className={className}>
         <AvatarImage src={player.image_url ?? ""} />
-        <AvatarFallback>{player.name.slice(0, 2).toUpperCase()}</AvatarFallback>
+        <AvatarFallback delayMs={10}>{player.name.slice(0, 2).toUpperCase()}</AvatarFallback>
       </Avatar>
       {showCrown && (
         <Crown className="absolute -top-[20px] left-[20px] rotate-[25deg]" />
